72b8abf4ee49cc5da32bcc536840cb36f7dea80e
ganeshgunaki <ganu024@gmail.com>
2018-06-29 12:09:24 +0530

    Update TestBase.java

--- SeleniumHybridFramework/src/com/actiTime/testBase/TestBase.java
+++ SeleniumHybridFramework/src/com/actiTime/testBase/TestBase.java
@@ -140,3 +140,210 @@ public class TestBase extends Utills{
 		return sampleData;
 	}
 }
+/////////////55555
+package utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import config.Constant;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestBase {
+
+	public static Properties Repository = new Properties();
+	public static File f;
+	public static FileInputStream FI;
+	public WebElement element = null;
+	public static String MobileOSValue;
+	public static WebDriver driver = null;
+
+	@BeforeTest()
+	@Parameters({ "browser", "MobileOS" })
+	public static void beforeTest(String browser, String MobileOS) throws Exception {
+		MobileOSValue = MobileOS;
+		loadPropertiesFile();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		try {
+			switch (browser) {
+			case "Desktop Chrome":
+				System.setProperty("webdriver.chrome.driver",
+						"C:\\Automation\\SeleniumAutomation\\Practice\\src\\main\\java\\resources\\drivers\\chromedriver_2.32.exe");
+				ChromeOptions options = new ChromeOptions();
+				options.setExperimentalOption("useAutomationExtension", false);
+				driver = new ChromeDriver(options);
+				// driver= new RemoteWebDriver(new
+				// URL("http://127.0.0.1:8080/wd/hub"), capabilities);
+				break;
+
+			case "Desktop Firefox": // omni-channel
+				break;
+			}
+
+			capabilities.setCapability("scriptName", "Give Script Name");
+
+		} catch (Exception e) {
+
+			throw e;
+		}
+	}// end of method beforeTest
+
+	protected TestBase() {
+
+	}
+
+	public static void loadPropertiesFile() throws IOException {
+
+		f = new File(
+				System.getProperty("user.dir") + "\\src\\main\\java\\locators\\" + MobileOSValue + "\\obj.properties");
+		FI = new FileInputStream(f);
+		Repository.load(FI);
+
+		// f = new
+		// File(System.getProperty("user.dir")+"\\src\\com\\actiTime\\pageLocators\\loginpage.properties");
+		// FI = new FileInputStream(f);
+		// Repository.load(FI);
+		//
+		// f = new
+		// File(System.getProperty("user.dir")+"\\src\\com\\actiTime\\pageLocators\\reportsPage.properties");
+		// FI = new FileInputStream(f);
+		// Repository.load(FI);
+		//
+		// f = new
+		// File(System.getProperty("user.dir")+"\\src\\com\\actiTime\\pageLocators\\timeTracks.properties");
+		// FI = new FileInputStream(f);
+		// Repository.load(FI);
+		//
+		// f = new
+		// File(System.getProperty("user.dir")+"\\src\\com\\actiTime\\pageLocators\\taskPage.properties");
+		// FI = new FileInputStream(f);
+		// Repository.load(FI);
+		//
+		// /*f = new
+		// File(System.getProperty("user.dir")+"\\src\\Config\\AppText.properties");
+		// FI = new FileInputStream(f);
+		// AppText.load(FI); */
+
+	}
+
+	public static WebElement getLocator(String locator) throws Exception {
+		String[] split = locator.split(":");
+		String locatorType = split[0];
+		String locatorValue = split[1];
+
+		if (locatorType.toLowerCase().equals("id"))
+			return driver.findElement(By.id(locatorValue));
+		else if (locatorType.toLowerCase().equals("name"))
+			return driver.findElement(By.name(locatorValue));
+		else if ((locatorType.toLowerCase().equals("classname")) || (locatorType.toLowerCase().equals("class")))
+			return driver.findElement(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("tagname")) || (locatorType.toLowerCase().equals("tag")))
+			return driver.findElement(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("linktext")) || (locatorType.toLowerCase().equals("link")))
+			return driver.findElement(By.linkText(locatorValue));
+		else if (locatorType.toLowerCase().equals("partiallinktext"))
+			return driver.findElement(By.partialLinkText(locatorValue));
+		else if ((locatorType.toLowerCase().equals("cssselector")) || (locatorType.toLowerCase().equals("css")))
+			return driver.findElement(By.cssSelector(locatorValue));
+		else if (locatorType.toLowerCase().equals("xpath"))
+			return driver.findElement(By.xpath(locatorValue));
+		else
+			throw new Exception("Unknown locator type '" + locatorType + "'");
+	}
+
+	public static List<WebElement> getLocators(String locator) throws Exception {
+		String[] split = locator.split(":");
+		String locatorType = split[0];
+		String locatorValue = split[1];
+
+		if (locatorType.toLowerCase().equals("id"))
+			return driver.findElements(By.id(locatorValue));
+		else if (locatorType.toLowerCase().equals("name"))
+			return driver.findElements(By.name(locatorValue));
+		else if ((locatorType.toLowerCase().equals("classname")) || (locatorType.toLowerCase().equals("class")))
+			return driver.findElements(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("tagname")) || (locatorType.toLowerCase().equals("tag")))
+			return driver.findElements(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("linktext")) || (locatorType.toLowerCase().equals("link")))
+			return driver.findElements(By.linkText(locatorValue));
+		else if (locatorType.toLowerCase().equals("partiallinktext"))
+			return driver.findElements(By.partialLinkText(locatorValue));
+		else if ((locatorType.toLowerCase().equals("cssselector")) || (locatorType.toLowerCase().equals("css")))
+			return driver.findElements(By.cssSelector(locatorValue));
+		else if (locatorType.toLowerCase().equals("xpath"))
+			return driver.findElements(By.xpath(locatorValue));
+		else
+			throw new Exception("Unknown locator type '" + locatorType + "'");
+	}
+
+	public WebElement getWebElement(String locator) throws Exception {
+		return getLocator(Repository.getProperty(locator));
+	}
+
+	public List<WebElement> getWebElements(String locator) throws Exception {
+		return getLocators(Repository.getProperty(locator));
+	}
+
+	public boolean clickAction(String locator) {
+
+		try {
+			element = getWebElement(locator);
+		} catch (Exception e1) {
+
+			e1.printStackTrace();
+		}
+		WebDriverWait wait = new WebDriverWait(driver, Constant.WEBDRIVER_WAIT);
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		try {
+			element.click();
+			Thread.sleep(2000);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+	public static void openURL() throws MalformedURLException, AssertionError, NoSuchElementException, Exception {
+		System.out.println("\n Launching: " + Constant.url + " now...");
+		try {
+			driver.get(Constant.url);
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Constant.IMPLICIT_WAIT, TimeUnit.SECONDS);
+			System.out.println("- - Open Home Page URL ");
+
+		} catch (Exception ex) {
+			System.out.println("\n Unable to open URL: " + Constant.url + "not sure why" + ex);
+		}
+	}
+
+	public boolean setText(WebElement element, String testData) {
+		WebDriverWait wait = new WebDriverWait(driver, Constant.IMPLICIT_WAIT);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		try {
+			element.sendKeys(testData);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	} // end of method end of setText
+
+}
